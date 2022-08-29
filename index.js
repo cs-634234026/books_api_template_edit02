73ea5635fc3041d1c52a772573f0480a72d2e585
3estx <634234026@parichat.skru.ac.th>
2022-08-29 08:46:14 +0700

    create

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var cors = require("cors");
+
+require('dotenv').config({
+  path: `config/.env`
+})
+
+api_version=process.env.api_version
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Application listening at http://localhost:${port}`)
+});
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.use(cors());
+
+require('./routes/index')(app)
